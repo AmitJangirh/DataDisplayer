d06fce2f2b2147a80e4e80ffcc6084a8b9f4c34f
Amit Jangirh <amitjangirh@gmail.com>
2021-06-14 17:31:11 +0530

    Delete .DS_Store

--- iOSApp/iOSApp/ViewModel.swift
+++ iOSApp/iOSApp/ViewModel.swift
@@ -24,24 +24,21 @@ import Foundation
 // MARK: - Section Data
 struct SectionData {
     var title: String
-    var rowData: RowData
+    var rowData: [StringConvertable]
 }
 
-// MARK: - Row Data
-struct RowData {
-    var rowDataArray: [String]
-}
+// MARK: - StringConvertable
 
-protocol Data {
-    associatedtype RawData
-    var rawValue: RawData { get set }
+protocol StringConvertable {
     var stringValue: String { get }
 }
 
-//protocol RowData {
-//    associatedtype Data
-//    var dataArray: [Data] { get set }
-//}
+// MARK: - Row Data
+
+protocol RowData: StringConvertable {
+    associatedtype RawData
+    var rawValue: RawData { get set }
+}
 
 // MARK: - ViewModel
 struct ViewModel {
@@ -56,7 +53,7 @@ struct ViewModel {
     }
     
     func rowCount(at section: Int) -> Int {
-        return self.sectionArray[section].rowData.rowDataArray.count
+        return self.sectionArray[section].rowData.count
     }
     
     subscript(section section: Int) -> SectionData {
@@ -64,15 +61,65 @@ struct ViewModel {
     }
     
     subscript(valueAt indexPath: IndexPath) -> String {
-        let data = self[section: indexPath.section].rowData.rowDataArray[indexPath.row]
-        return "\(data)"
+        let data = self[section: indexPath.section].rowData[indexPath.row]
+        return data.stringValue
     }
 }
 
 // MARK: - Sample Data
-let alphabetArray = RowData(rowDataArray: ["A", "B", "C", "D"])
-let intArray = RowData(rowDataArray: ["1", "2", "3", "4"])
+struct IntData: RowData {
+    typealias RawData = Int
+    var rawValue: Int
+    
+    var stringValue: String {
+        return String(rawValue)
+    }
+    
+    static var dataArray: [IntData] {
+        [IntData(rawValue: 1),
+         IntData(rawValue: 2),
+         IntData(rawValue: 3),
+         IntData(rawValue: 4)]
+    }
+}
 
-var sections: [SectionData] = [SectionData(title: "Alphabet", rowData: alphabetArray),
-                               SectionData(title: "Numeric", rowData: intArray)]
+struct CharData: RowData {
+    typealias RawData = Character
+    var rawValue: Character
+    
+    var stringValue: String {
+        return "\(rawValue)"
+    }
+    
+    static var dataArray: [CharData] {
+        [CharData(rawValue: "A"),
+         CharData(rawValue: "B"),
+         CharData(rawValue: "C"),
+         CharData(rawValue: "D")]
+    }
+}
 
+struct EmployeeData {
+    var name: String
+    var id: Int
+}
+
+struct CustomData: RowData {
+    typealias RawData = EmployeeData
+    var rawValue: EmployeeData
+    
+    var stringValue: String {
+        return "Name: " + rawValue.name + ", id: \(rawValue.id)"
+    }
+    
+    static var dataArray: [CustomData] {
+        [CustomData(rawValue: EmployeeData(name: "AJ", id: 123)),
+         CustomData(rawValue: EmployeeData(name: "DL", id: 123)),
+         CustomData(rawValue: EmployeeData(name: "LALA", id: 123)),
+         CustomData(rawValue: EmployeeData(name: "OK", id: 123))]
+    }
+}
+
+var sections: [SectionData] = [SectionData(title: "Alphabet", rowData: CharData.dataArray),
+                               SectionData(title: "Numeric", rowData: IntData.dataArray),
+                               SectionData(title: "EmployeeData", rowData: CustomData.dataArray)]
